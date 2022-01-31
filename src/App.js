b95d136520be825be44adf6c3526a2f4d811a080
vdiz510 <vernchiem2000@gmail.com>
2022-01-30 16:52:12 -0800

    Shift navbar to right, add placeholder bg image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import { render } from '@testing-library/react';
 import React from 'react'
 import NavBar from './components/NavBar'
+import './components/App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div><NavBar /></div>
+      <div className="ui container navbar">
+        <NavBar />
+      </div>
     )
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const NavBar = () => {
+class NavBar extends React.Component {
+  render() {
     return (
-        <div>NavBar</div>
+      // <div className="ui menu">
+      //   <a className="item">Home</a>
+      //   <div className="ui pointing dropdown link item visible active">
+      //     <span className="text">Dropdown Test</span>
+      //     <i className="dropdown icon"></i>
+      //     <div className="menu">
+      //       <div className="header">Categories</div>
+      //       <div className="item">About</div>
+      //     </div>
+      //   </div>
+      // </div>
+
+      <div className="ui secondary menu navbar">
+        <div className="right menu">
+          <a className="active item">Home</a>
+          <a className="item">About Me</a>
+          <a className="item">Experience</a>
+          <a className="item">Socials</a>
+        </div>
+      </div>
     )
+  }
 }
 
 export default NavBar;
